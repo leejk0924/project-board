4bbce309efc2486d2ad98b842be0f7606bd2c7f5
jk <akdirdl@gmail.com>
2024-05-07 00:55:08 +0900

    feat: Entity 설정

--- src/main/java/com/jk/projectboard/domain/Article.java
+++ src/main/java/com/jk/projectboard/domain/Article.java
@@ -1,15 +1,79 @@
 package com.jk.projectboard.domain;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy"),
+})
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter
+    @Column(nullable = false)
     private String title;
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content;
+    @Setter
     private String hashtag;
 
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createAt;
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;
+
+    // 하이버네이트 구현자를 가질 때 기본생성자를 가지고 있어야함
+    protected Article() {
+    }
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+    // equals , hashCode 구현 (동일성, 동등성 검사를 위해)
+    // DB에서 id가 동일하면 동등성, 동일성이 적합하다 (장점 : 성능을 높일 수 있음, 고로 롬복 사용 x)
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        // 영속화 되지 않았다면 다른 값으로 취급함
+        return id != null && id.equals(article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
