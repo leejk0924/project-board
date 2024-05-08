b296e5af0203a3c141310d7eb17d77a4418ab5ab
jk <akdirdl@gmail.com>
2024-05-09 00:13:21 +0900

    test: test DB 설정

--- src/test/java/com/jk/projectboard/repository/JpaRepositoryTest.java
+++ src/test/java/com/jk/projectboard/repository/JpaRepositoryTest.java
@@ -5,8 +5,10 @@ import com.jk.projectboard.domain.Article;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -14,8 +16,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 // junit5에서는 테스트에서도 생성자 주입 가능 -> @DataJpaTest 에 @ExtendWith(SpringExtension.class) 가 있음 -> Autowired 로직 있음
 @DisplayName("JPA 연결 테스트")
+// spring:config.activate.on-profile 이 testdb 인 것을 설정으로 사용
+@ActiveProfiles("testdb")
+// 2. Test DB 를 따로 불러오지 않고 application.yaml 에 설정되어있는 DB를 불러온다.
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(JpaConfig.class)
 // @DataJpaTest ->  @Transactional 어노테이션 포함
+// 1. 아래 어노테이션이 Embedded database 불러옴
 @DataJpaTest
 class JpaRepositoryTest {
     private ArticleRepository articleRepository;
