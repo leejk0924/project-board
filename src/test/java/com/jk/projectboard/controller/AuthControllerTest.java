debeb47b7b0430fbb923d2d573113944776d87d6
jk <akdirdl@gmail.com>
2024-09-26 01:38:38 +0900

    실패하는 테스트 케이스 수정

--- src/test/java/com/jk/projectboard/controller/AuthControllerTest.java
+++ src/test/java/com/jk/projectboard/controller/AuthControllerTest.java
@@ -1,24 +1,31 @@
 package com.jk.projectboard.controller;
 
-import com.jk.projectboard.config.SecurityConfig;
+import com.jk.projectboard.config.TestSecurityConfig;
+import com.jk.projectboard.service.ArticleService;
+import com.jk.projectboard.service.PaginationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("View 컨트롤러 - 인증")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(Void.class)
 public class AuthControllerTest {
     private final MockMvc mvc;
+    @MockBean
+    private ArticleService articleService;
+    @MockBean
+    private PaginationService paginationService;
 
     public AuthControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -28,7 +35,8 @@ public class AuthControllerTest {
     public void givenNothing_whenTryingToLogin_thenReturnLoginView() throws Exception {
         mvc.perform(get("/login"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+        then(articleService).shouldHaveNoInteractions();
+        then(paginationService).shouldHaveNoInteractions();
     }
 }
